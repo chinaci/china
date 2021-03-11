30a4b53456123cfaabac8bd080a3a1d3b909bb2f
60kr <79446776+60kr@users.noreply.github.com>
2021-03-11 21:07:17 +0800

    Delete templates/article/touch directory

--- templates/article/touch/js/speclist.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    if($.browser.msie && parseInt($.browser.version) >= 8){
-        $('.listBox ul li:nth-child(3n)').css('margin-right','0');
-    }
-    $('.filter .fil span').click(function(){
-        $(this).addClass('curr').siblings().removeClass('curr');
-    });
-});
--- templates/article/touch/js/top.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $('.header .wx_box').on('mouseover',function () {
-        $('.header .wx_img').addClass('show');
-    });
-    $('.header .wx_box').on('mouseout',function () {
-        $('.header .wx_img').removeClass('show');
-    });
-});
